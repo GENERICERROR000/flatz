6cb7036969e553fa21894125c920be4470450597
nkernis <nkernis@gmail.com>
2017-07-10 17:01:51 -0400

    server setup and client creation

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      # TODO: Add Routes
+      # EXAMPLE: resources :nodes, except: [:index, :new, :edit, :update]
+    end
+  end
 end
