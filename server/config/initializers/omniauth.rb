3d237e697c56b455766cdcc7569cf5a490c1985b
Jack H <jackhe678@gmail.com>
2017-07-13 14:04:30 -0400

    User find or create completed and working

--- server/config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :slack, '2727337933.211352935332', 'f4e1856f6a0d49a1c34c3c0eca6249ad', scope: 'identity.basic'
-end
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+jack = User.create(email: 'jack@jack.com', uid: 'abcd12345', name: 'jack')
