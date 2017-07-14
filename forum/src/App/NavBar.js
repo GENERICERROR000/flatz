7f19b6f3ad35166822d19a47632274efb1e82f03
Jack H <jackhe678@gmail.com>
2017-07-14 11:22:16 -0400

    trying to fix forum

---
+++ forum/src/App/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div className="ui pointing menu">
+        <a href="http://localhost:3001" className="item">
+          Home
+        </a>
+        <a className="item">
+          Profile
+        </a>
+        <a href="http://localhost:4000" className="item">
+          Flatz Board
+        </a>
+        <div className="right menu">
+          {localStorage.jwt ? <a className="item" onClick={this.props.logout}>Sign Out</a> : <a className="item">Click Dat Slack Button</a>}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Navbar
---
+++ forum/src/config.js
@@ -0,0 +1,5 @@
+import { projects } from 'json!../.firebaserc'
+
+export const apiKey = "AIzaSyBz6mA2s2K3NXw9feX2Y18FA4Ys0QE_s-s"
+export const projectId = projects.default
+export const siteName = "Flatz Event Search"
